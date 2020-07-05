932abef2579d3c176935872f382e857a99bc00d4
Amirreza KN <amirrezanochi@gmail.com>
2020-07-05 14:50:44 +0430

    simple web server

--- micro/main.go
+++ micro/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	_ "log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":9000", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "oopsi", http.StatusBadRequest)
+		// w.WriteHeader(http.StatusBadRequest)
+		// w.Write([]byte("oopsi"))
+		return
+	}
+
+	fmt.Fprintf(w, "Hello %s\n", data)
 }
